8a0263eae8c7634ede798b710794798739aa1112
juanito15 <juanisantacruz@gmail.com>
2014-02-22 22:14:00 +0000

    Form

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -4,5 +4,23 @@ def index
 	@pics = Pic.all
 end
 
+	def new
 
+		@pic = Pic.new
+
+
+	end
+		
+		def create
+			Pic.create( pic_params )
+			redirect_to pics_path
+			 
+
+		end
+
+		private
+
+		def pic_params
+			params.require(:pic).permit(:title, :subtitle, :message)
+		end
 end
